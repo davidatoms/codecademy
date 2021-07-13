2418b94252d4ca15e5b2e0fa13dc356d4f99ee59
davidatoms <davidmichaeladams94@gmail.com>
2021-07-12 23:59:26 -0600

    Completed project

--- the_fender/script.py
+++ the_fender/script.py
@@ -1,10 +1,48 @@
 import csv
-
+import json
 
 compromised_users = []
 
-with open('passwords.csv') as password_file:
-    password_csv - csv.DictReader(password_file)
-    for row in password_csv:
-        print(row['Username'])
-    
+# Step number 5
+with open('the_fender/passwords.csv') as password_file:
+    password_csv = csv.DictReader(password_file)
+    for password_row in password_csv:
+        compromised_users.append(password_row['Username'])
+
+# Step number 7
+# print(compromised_users)
+
+# Step number 8
+with open('compromised_users.txt', 'w') as compromised_user_file:
+    for compromised_user in compromised_users:
+        compromised_user_file.write(compromised_user)
+
+# Step number 11
+with open('boss_message.json', 'w') as boss_message:
+    boss_message_dict = {
+        'recipient': 'The Boss',
+        'message': 'Mission Success'
+    }
+    json.dump(boss_message_dict, boss_message)
+
+# Step number 16 
+with open('the_fender/new_passwords.csv', 'w') as new_passwords_obj:
+    slash_null_sig = """
+     _  _     ___   __  ____             
+    / )( \   / __) /  \(_  _)            
+    ) \/ (  ( (_ \(  O ) )(              
+    \____/   \___/ \__/ (__)             
+    _  _   __    ___  __ _  ____  ____  
+    / )( \ / _\  / __)(  / )(  __)(    \ 
+    ) __ (/    \( (__  )  (  ) _)  ) D ( 
+    \_)(_/\_/\_/ \___)(__\_)(____)(____/ 
+            ____  __     __   ____  _  _ 
+    ___   / ___)(  )   / _\ / ___)/ )( \
+    (___)  \___ \/ (_/\/    \\___ \) __ (
+        (____/\____/\_/\_/(____/\_)(_/
+    __ _  _  _  __    __                
+    (  ( \/ )( \(  )  (  )               
+    /    /) \/ (/ (_/\/ (_/\             
+    \_)__)\____/\____/\____/
+    """
+    new_passwords_obj.write(slash_null_sig)
