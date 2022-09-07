3e75c277e914de214c545a0c823daaa088b03057
David Adams <david_mi_adams@outlook.com>
2022-09-07 06:34:57 -0600

    created initials with py

---
+++ financial-analysis-with-python/ascii.py
@@ -0,0 +1,9 @@
+# david adams
+# learning to code with codecademy
+# ascii art my initials
+print("DDDD  M    M   A  ")
+print("D   D MM  MM  AAA ")
+print("D   D M MM M A   A")
+print("D   D M    M AAAAA")
+print("D   D M    M A   A")
+print("DDDD  M    M A   A")
